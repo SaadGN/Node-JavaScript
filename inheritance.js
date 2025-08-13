a6e46afe0e4939dc15b42a5a878657bd399e6588
saad <saadgn89@gmail.com>
2025-08-14 01:25:29 +0500

    inheritance practice code

---
+++ inheritance.js
@@ -0,0 +1,37 @@
+
+console.log("*****************************************");
+console.log("             INHERITANCE");
+console.log("*****************************************\n");
+
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const user1 = new Teacher("John", "john@teacher.com", "123")
+
+user1.logMe()
+const user2 = new User("user2")
+
+user2.logMe()
+
+
+console.log(user1 === User);
+console.log(user1 instanceof User);
